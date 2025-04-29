e5c741aa83876c645cd5fe25f0b18a6802cf1421
Xsemka <tima.glavatskikh.10@mail.ru>
2025-04-29 18:08:01 +0400

    Add files via upload

--- DbT/user/user.py
+++ DbT/user/user.py
@@ -1,19 +1,28 @@
 import socket
+import json
+import rsa
 
-#написть декоратор для шифрования
+
 
 class Client:
     def __init__(self, host, port):
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clientSocket.connect((host, port))
+        self.clientSocket.send("1".encode())
+        self.publicKey = self.clientSocket.recv(4096)
 
 class Authorization(Client):
     def registrarion(self, login, password, staffRights = False):
-        self.clientSocket.send(f"{"registrarion"} {login} {password} {staffRights}".encode())
+        self.clientSocket.send(rsa.encrypt(json.dumps({"action":"registrarion", "login":login, "password":password, "staffRights":staffRights}, ensure_ascii=False, indent=2).encode(), self.publicKey))
 
     def login(self, login, password):
-        self.clientSocket.send(f"{"login"} {login} {password}".encode())
+        self.clientSocket.send(json.dumps({"action":"login","login":login, "password":password}, ensure_ascii=False, indent=2).encode())
 
 class OrderManager(Client):
     def SendOrder(self, orderId, carriageNumber, placeNumber, dishesId, status = "notStarted"):
-        self.clientSocket.send(f"{"order"} {orderId} {carriageNumber} {placeNumber} {dishesId} {status}".encode())
+        self.clientSocket.send(json.dumps({"action":"order", "orderId":orderId, "carriageNumber":carriageNumber, "placeNumber":placeNumber, "dishesId":dishesId, "status":status}, ensure_ascii=False, indent=2).encode())
+
+
+
+
+
